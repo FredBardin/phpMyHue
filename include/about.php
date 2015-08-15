1192b31a1452d0fea0e1dcf4dcba626d19c11496
Frederic Bardin <bardin.frederic@gmail.com>
2015-08-15 18:38:27 +0200

    v1.2.3

--- include/about.php
+++ include/about.php
@@ -8,6 +8,9 @@ if (! defined('ANTI_HACK')){exit;}
 <SCRIPT language="javascript">
 $('#detail').hide("slide");
 </SCRIPT>
-<DIV ID=about>phpMyHue 1.2.2<BR>
+<DIV ID=about>phpMyHue 1.2.3<BR>
 &copy; <A HREF="mailto:bardin.frederic@gmail.com" CLASS=about>F. Bardin</A> 07-2015<BR>
-<small>Current language : "<?php echo $lang?>"</small></DIV>
+<small>
+Bridge IP : <?php echo $bridgeip?><BR>
+Current language : <?php echo $lang?>
+</small></DIV>

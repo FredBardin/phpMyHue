b74344584b88905f755c82a630fccc722c0d131e
Frederic Bardin <bardin.frederic@gmail.com>
2015-11-07 15:31:47 +0100

    sync pb

--- include/about.php
+++ include/about.php
@@ -5,20 +5,13 @@
 // Anti-hack
 if (! defined('ANTI_HACK')){exit;}
 
-include 'include/functions.php';
 ?>
 <SCRIPT language="javascript">
 $('#detail').hide("slide");
 </SCRIPT>
-<<<<<<< HEAD
 <DIV ID=about>phpMyHue 1.3<BR>
 &copy; <A HREF="mailto:bardin.frederic@gmail.com" CLASS=about>F. Bardin</A> 09-2015<BR>
 <small>
 Bridge IP : <?php echo $bridgeip?><BR>
-Current language : <?php echo $lang;?>
+Current language : <?php echo $lang?>
 </small></DIV>
-=======
-<DIV ID=about>phpMyHue 1.2.2<BR>
-&copy; <A HREF="mailto:bardin.frederic@gmail.com" CLASS=about>F. Bardin</A> 07-2015<BR>
-<small>Current language : "<?php echo $lang?>"</small></DIV>
->>>>>>> refs/remotes/origin/master

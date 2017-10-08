0be7ea7c5eb9892c3b7695eea017d2f579344d2f
Frederic Bardin <bardin.frederic@gmail.com>
2017-10-08 10:35:42 +0200

    v1.6a

--- include/config_org.php
+++
@@ -1,9 +0,0 @@
-<?php
-if (! defined('ANTI_HACK')){exit;}
-/*****************
- * Configuration *
- *****************/
-$bridgeip = "192.168.0.17";
-$username = "9fe726a16b88b771636ccd623ab4093";
-$lang = "fr";
-?>
